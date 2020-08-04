c34d46a483db3e74e22e066206ba080e7efa788d
Meghdut Mandal <meghdut.windows@gmail.com>
2020-08-04 10:36:31 +0530

    Slight change

--- lab2/Q15.R
+++ lab2/Q15.R
@@ -1,6 +1,6 @@
 # Title     : TODO
 # Objective : TODO
 # Created by: KIIT
-# Created on: 04-08-2020
-
+# Created on: 8/4/2020
+n3 <- 34
 rm(n3)
--- lab2/q4.R
+++ lab2/q4.R
@@ -6,8 +6,8 @@
 a <- as.integer(readline("Enter the  number "))
 b <- as.integer(readline("Enter the  number "))
 
-max<-max(a,b)
+max <- max(a, b)
 min <- min(a, b)
 
-cat("the  max is ",max)
-cat(" the min is ",min)
+cat("the  max is ", max)
+cat(" the min is ", min)
