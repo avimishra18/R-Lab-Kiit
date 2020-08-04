ad745f183df693ea318543fc08d3cdbddd588ace
curatorRaj <1828043@kiit.ac.in>
2020-08-04 09:56:54 +0530

    Q5

--- lab2/howManyDigit.R
+++ lab2/howManyDigit.R
@@ -2,4 +2,17 @@
 # Objective : TODO
 # Created by: KIIT
 # Created on: 04-08-2020
+n <- as.integer(readline(prompt = "enter a no"))
+if(n%%10==n){
+  cat("no of digit is",1)
+}else if(n%%100==n){
+  cat("no of digit is",2)
+}else if(n%%1000==n){
+  cat("no of digit is",3)
+}else if(n%%10000==n){
+  cat("no of digit is",4)
+}else if(n%%100000==n){
+  cat("no of digit is",5)
+}else
+  print("no of digit is greater than 5")
 
