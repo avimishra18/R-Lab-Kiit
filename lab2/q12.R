aec3b5c8a5d1769805e37ac914ba7773352da3d7
Meghdut Mandal <meghdut.windows@gmail.com>
2020-08-04 10:06:12 +0530

    Done q12

---
+++ lab2/q12.R
@@ -0,0 +1,11 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: KIIT
+# Created on: 8/4/2020
+
+logical <- TRUE
+number <- 23.5
+complex <- 2+5i
+charec <- "TRUE"
+
+cat(" logcal ",logical," number ",number,"  complex ",complex,"  charecter ",charec)
