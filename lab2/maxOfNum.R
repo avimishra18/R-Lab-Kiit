7f41b85c57a293a5d5b41c259fb0d63d5e4dec0b
curatorRaj <1828043@kiit.ac.in>
2020-08-04 09:50:11 +0530

    odd or even

--- lab2/maxOfNum.R
+++ lab2/maxOfNum.R
@@ -7,7 +7,7 @@ n2 <- as.integer(readline("enter 2nd no"))
 if(n1>n2){
   cat("gretest no is",n1)
 }else if(n2>n1){
-  cat("gretest no is",n1)
+  cat("gretest no is",n2)
 }else
   cat("no is equal")
 
