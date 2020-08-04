77f6670399a10fb2aa9bc7ba0ed186c774a2d733
Meghdut Mandal <meghdut.windows@gmail.com>
2020-08-04 09:50:18 +0530

    Done q2

---
+++ lab2/q2.R
@@ -0,0 +1,13 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: KIIT
+# Created on: 8/4/2020
+
+a <- as.integer(readline("Enter the  number "))
+
+if (a > 0) {
+  print(" Number is positive ")
+}else  {
+  print("Number is negetive \n")
+}
+
---
+++ lab2/q4.R
@@ -0,0 +1,10 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: KIIT
+# Created on: 8/4/2020
+
+a <- as.integer(readline("Enter the  number "))
+b <- as.integer(readline("Enter the  number "))
+c <- as.integer(readline("Enter the  number "))
+d <- as.integer(readline("Enter the  number "))
+
