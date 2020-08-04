6e0e13f88118f039017e4f1c1441056f4f5df390
curatorRaj <1828043@kiit.ac.in>
2020-08-04 09:48:14 +0530

    odd or even

---
+++ lab2/oddOrEven.R
@@ -0,0 +1,10 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: KIIT
+# Created on: 04-08-2020
+n <- as.integer(readline(prompt = "enter a no"))
+if(n%%2==0){
+  cat("no is even")
+}else
+  cat("no is odd")
+
