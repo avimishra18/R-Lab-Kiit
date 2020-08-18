7d7fab495098c3772bf3a923aa4cec2f98a8e984
Meghdut Mandal <meghdut.windows@gmail.com>
2020-08-18 10:43:07 +0530

    q9 fixed

--- lab3/q5.R
+++ lab3/q5.R
@@ -5,6 +5,8 @@ reverse <- 0
 rem <- 0
 sumDig <- 0
 
+number<- n
+
 while (n > 0)
 {
   rem <- n %% 10
@@ -13,16 +15,16 @@ while (n > 0)
   reverse <- reverse * 10 + rem;
   n <- n %/% 10;
 }
-
+cat("Sum of the digits are ",sumDig,"\n")
 cat("The reversed number is ", reverse, "\n")
 
-cat("The factors of the number is ")
+print("The factors of the number are")
+print(number)
 
-fact <- 1
-while (fact <= n) {
-  if (n %% fact == 0) {
-    cat(" ", fact)
+i<- 1
+while (i < number) {
+  if (number %% i == 0) {
+    print(i)
   }
+  i <- i + 1
 }
-
-cat("\nSum of the digits are ",sumDig)
