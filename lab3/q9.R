cff607db09dff6b2f749ea2acfe8c80c43b22899
Meghdut Mandal <meghdut.windows@gmail.com>
2020-08-18 10:16:34 +0530

    Q9 Done

--- lab3/q9.R
+++ lab3/q9.R
@@ -6,12 +6,26 @@
 n <- as.integer(readline("Enter the  Number  "))
 reverse <- 0
 rem <- 0
+sumDig <- 0
 
 while (n > 0)
 {
-  rem <- n %% 10;
+  rem <- n %% 10
+  sumDig <- sumDig + rem
+
   reverse <- reverse * 10 + rem;
-  n <- n  %/% 10;
+  n <- n %/% 10;
 }
 
-cat("The reversed number is ",reverse)
+cat("The reversed number is ", reverse, "\n")
+
+cat("The factors of the number is ")
+
+fact <- 1
+while (fact <= n) {
+  if (n %% fact == 0) {
+    cat(" ", fact)
+  }
+}
+
+cat("\nSum of the digits are ",sumDig)
