072d65c133c70d4609bc838912859be4955be72c
curatorRaj <1828043@kiit.ac.in>
2020-08-25 10:06:31 +0530

    4

--- Lab 4/Q4.R
+++ Lab 4/Q4.R
@@ -2,5 +2,12 @@
 # Objective : TODO
 # Created by: KIIT
 # Created on: 25-08-2020
-string <- "abhishek raj"
-print(string.)
+string <- "mainak"
+print(nchar(string))#count no of character
+print(paste("R","class","is ","very","boaring"))#concatenate String
+sprintf("%s scored %.2f percent", string, 72.3)#c style printing
+cat(substr(string,2,4))#substring
+print(strsplit("today is monday"," "))
+
+
+
