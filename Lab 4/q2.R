89cc89e5c2dcbe5e22293d61b580fafc1efad455
Meghdut Mandal <meghdut.windows@gmail.com>
2020-08-25 10:52:54 +0530

    q2 rollback

--- Lab 4/q2.R
+++ Lab 4/q2.R
@@ -3,32 +3,6 @@
 # Created by: KIIT
 # Created on: 8/25/2020
 
+x <- matrix(c(1, 0, 2, -1,3, 0, 0, 5,2, 1, 4, -3,1, 0, 5, 0), 4, 4)
 
-get_cofactor <- function(matrix, temp, p, q) {
-  i <- 0;
-  j <- 0;
-  for(row in 0:(n-1)){
-     for( col in 0:(n-1)){
-
-       if (row != p && col != q){
-         temp[i,j] <- matrix[row,col];
-         j<-j+1;
-         if (j == n - 1){
-           j<-0
-           i<-i+1
-         }
-       }
-     }
-  }
-}
-
-determinant<- function (matrix,n){
-  d<-0;
-  if(n==1){
-    v<- matrix[0,0];
-    v
-  }
-
-}
-
-
+det(x)
